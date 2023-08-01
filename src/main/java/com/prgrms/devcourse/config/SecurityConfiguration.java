566db014b46dd639c1069e12900b95f62e65fbef
Yiseul Park <omjl5123@catholic.ac.kr>
2023-08-01 16:27:47 +0900

    feat: 사용자 계정 정보 추가

--- src/main/java/com/prgrms/devcourse/config/SecurityConfiguration.java
+++ src/main/java/com/prgrms/devcourse/config/SecurityConfiguration.java
@@ -3,6 +3,10 @@ package com.prgrms.devcourse.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +24,17 @@ public class SecurityConfiguration {
                         .permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("user123")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
