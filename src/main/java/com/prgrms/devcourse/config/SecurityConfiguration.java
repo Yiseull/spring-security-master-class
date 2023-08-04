533d69d053ff2fddddd82b4dd5a9ddf97697562d
Yiseul Park <omjl5123@catholic.ac.kr>
2023-08-04 17:26:26 +0900

    feat: admin 페이지 접근 설정 추가

--- src/main/java/com/prgrms/devcourse/config/SecurityConfiguration.java
+++ src/main/java/com/prgrms/devcourse/config/SecurityConfiguration.java
@@ -1,14 +1,24 @@
 package com.prgrms.devcourse.config;
 
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
+import static org.springframework.security.authorization.AuthenticatedAuthorizationManager.fullyAuthenticated;
+import static org.springframework.security.authorization.AuthorityAuthorizationManager.hasRole;
+import static org.springframework.security.authorization.AuthorizationManagers.allOf;
+
+@Slf4j
 @Configuration
 public class SecurityConfiguration {
 
@@ -17,6 +27,7 @@ public class SecurityConfiguration {
         http
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/me").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/admin").access(allOf(fullyAuthenticated(), hasRole("ADMIN")))
                         .anyRequest().permitAll()
                 )
                 .formLogin(form -> form
@@ -31,6 +42,9 @@ public class SecurityConfiguration {
                 )
                 .requiresChannel(channel -> channel
                         .anyRequest().requiresSecure()
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .accessDeniedHandler(accessDeniedHandler())
                 );
 
         return http.build();
@@ -53,5 +67,19 @@ public class SecurityConfiguration {
                         .build();
 
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return ((request, response, e) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principle = authentication != null ? authentication.getPrincipal() : null;
+            log.warn("{}  is denied", principle, e);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain");
+            response.getWriter().write("## ACCESS DENIED ##");
+            response.getWriter().flush();
+            response.getWriter().close();
+        } );
     }
 }
